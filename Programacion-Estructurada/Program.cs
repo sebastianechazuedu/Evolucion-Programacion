a0bc2cbb06e46b2b5d90dd543d68cf8567394fea
Sebastian Echazú <sebastianechazu@outlook.com>
2020-05-05 07:07:04 -0300

    Primer commit

---
+++ Programacion-Estructurada/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programacion_Estructurada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
